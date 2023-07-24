24ea70ef8dce76a866daca0f8db9359149cb3dc9
nfernando0 <nandof986@gmail.com>
2023-07-24 15:43:26 +0700

    folder api

--- client/src/components/modal/ModalBox.jsx
+++ client/src/components/modal/ModalBox.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Modal } from 'react-bootstrap'
 import { useMutation } from 'react-query'
+import { API } from '../../config/api'
 
 function ModalBox({ show, handleClose, setDel }) {
 
